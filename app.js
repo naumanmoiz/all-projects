a3758a57989a8d6e947a4bef926b9066d6e65dc6
naumanmoiz <naumanmoiz@gmail.com>
2018-08-27 09:31:15 -0500

    Updated 08/27/2018

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require("express");
+var net = require('net');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -13,28 +15,37 @@ var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+require('events').EventEmitter.defaultMaxListeners = 3;
+
 mongoose.connect('process.env.MONGODB_URI');
 var db = mongoose.connection;
 
+var app     = express();
+var client = new net.Socket();
 
 
-// Init App
-var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-// View Engine
+app.use(express.static(__dirname + '/routes'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/public'));
+//Set Static Folder
+//app.use(express.static(path.join(__dirname, 'public')));
+//Store all JS and CSS in Scripts folder.
+//app.use(express.static(__dirname + '/views'));
+
+//View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
-// BodyParser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Express Session
+
+//Express Session
 app.use(session({
     secret: 'secret',
     saveUninitialized: true,
@@ -83,13 +94,95 @@ app.use(function(err, req, res, next) {
   });
 });
 
+/*app.use(function (req, res, next) {
+	  console.log('Time:', Date.now())
+	  next()
+	})*/
 
 
 app.use('/', routes);
 app.use('/users', users);
 
-// Set Port
-app.set('port', (process.env.PORT || 3000));
+
+
+
+
+
+/*app.get('/users/buckConverter', function(req, res) {
+	console.log("app.get users/buckConverter");
+	
+        res.sendFile(__dirname + "/" + "routes/BC1.html");
+    });*/
+
+
+
+app.post('/BC2', function(req, res){
+/*        req_input = {
+             Vin : req.body.Vin,
+              Du : req.body.Du,
+              Lu : req.body.Lu,
+              Cu : req.body.Cu,
+              Ru : req.body.Ru,
+              r1 : req.body.r1,
+              Fu : req.body.Fu
+              };
+
+        //this line is optional and will print the response on the command prompt
+        //It's useful so that we know what infomration is being transferred
+        //using the server
+        console.log(req_input);
+        console.log("Hi i am here");*/
+
+        //convert the response in JSON format
+      //  res.sendFile(__dirname + "/" + "routes/BC1.html");
+        //res.sendfile(JSON.stringify(response));
+        client.connect(9601, '127.0.0.1', function() {
+        	console.log('Connected');
+        	client.write(JSON.stringify(req_input));
+        });
+
+        // client.on('data', function(data) {
+        // 	console.log('Received: ' + data);
+        //   outputobj = JSON.parse(data);
+        //   console.log("Vo : " + outputobj.Vo);
+        //   console.log("Io : " + outputobj.Io);
+        //   console.log("P  : " + outputobj.P);
+        //   console.log("dIL: " + outputobj.deltaIL);
+        //   console.log(typeof(outputobj.deltaIL));
+        //   res.end(JSON.stringify(outputobj));
+        // 	client.destroy(); // kill client after server's response
+        // });
+
+      //  res.end(JSON.stringify(data));
+
+        client.on('close', function() {
+        	console.log('Connection closed');
+        });
+
+});
+
+
+/*
+function Juice() {
+
+console.log('Connected');
+
+//client.write(response);
+
+}
+*/
+
+
+app.get('/',function(req,res){
+	console.log('got into get function which is dir + index.shtml');
+  res.sendFile(__dirname + '/routes/index.shtml');
+  //It will find and locate index.html from View or Scripts
+});
+
+
+
+//Set Port
+app.set('port', (process.env.PORT || 80));
 
 app.listen(app.get('port'), function(err,next){
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,22 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express");
+var bodyParser = require('body-parser');
+var net = require('net');
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+var router = express.Router();
+
+var client = new net.Socket();
+var app     = express();
 
 /*
 
@@ -47,6 +62,10 @@ router.get('/people', function(req, res){
 router.get('/buckConverter', function(req, res){
 	console.log('got into buckConverter get function users.js');
 	res.render('buckConverter');
+});
+router.get('/solar', function(req, res){
+	console.log('got into solar get function users.js');
+	res.render('solar');
 });
 
 router.get('/about', function(req, res){
@@ -99,6 +118,9 @@ router.post('/register', function(req, res){
 	}
 });
 
+
+
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
 	  console.log('got into this method')
@@ -131,16 +153,126 @@ passport.deserializeUser(function(id, done) {
 });
 
 router.post('/login',
-  passport.authenticate('local', {failureRedirect:'/users/login',failureFlash: true}),
+  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
   function(req, res) {
 	console.log('got into router post login  function users & logged in successfully'+ req.user.username);
 	req.flash('success_msg', 'You are logged in');
 	
 	
-	res.render('index');
+	//res.render('login');
 	
-    //res.redirect('/');
+    res.redirect('/');
   });
+
+
+router.post('/buckConverter', function(req, res){
+	console.log("router post users/buckConverter")
+        req_input = {
+             Vin : req.body.Vin,
+              Du : req.body.Du,
+              Lu : req.body.Lu,
+              Cu : req.body.Cu,
+              Ru : req.body.Ru,
+              r1 : req.body.r1,
+              Fu : req.body.Fu
+              };
+
+        //this line is optional and will print the response on the command prompt
+        //It's useful so that we know what infomration is being transferred
+        //using the server
+        console.log(req_input);
+
+        //convert the response in JSON format
+      //  res.sendFile(__dirname + "/" + "routes/BC1.html");
+        //res.sendfile(JSON.stringify(response));
+        client.connect(6801, '127.0.0.1', function() {
+        	console.log('Connected');
+        	client.write(JSON.stringify(req_input));
+        	
+        });
+        
+        
+
+        client.on('data', function(data) {
+        	//console.log('Received :' + data);
+          //console.log(data +"before parse");
+          outputobj = JSON.parse(data);
+
+
+          console.log("Vo : " + outputobj["input cluster"].Vo);
+          var Vo = outputobj["input cluster"].Vo;
+          var Io = outputobj["input cluster"].Io;
+          var P = outputobj["input cluster"].P;
+          var deltaIL = outputobj["input cluster"].deltaIL;
+          var Time = outputobj.Time;
+          var outputData = outputobj["Outputs Data"];
+          console.log("Io : " + outputobj["input cluster"].Io);
+          console.log("P  : " + outputobj["input cluster"].P);
+          console.log("dIL: " + outputobj["input cluster"].deltaIL);
+          console.log("time" + outputobj.Time);
+          console.log("Outputs Data" + outputobj["Outputs Data"]);
+          
+//          google.charts.load('current', {packages: ['corechart']});
+//          google.charts.setOnLoadCallback(drawChart);
+//               function drawChart() {
+//                   // Define the chart to be drawn.
+//                   var data = new google.visualization.DataTable();
+//                   data.addColumn('string', 'Name');
+//                   data.addColumn('number', 'Values');
+//                   var i;
+//                   //values = $('#graphData').text.data1
+//          
+//                   for (i in outputobj.Time,outputObj["Outputs Data"]) {
+//                	   console.log(i + "this is I");
+//                	   console.log(i + "this is j");
+//                	   
+//                       data.addRows([[, data1[i]]]);
+//                   }
+//                   //j = "<h3>" + values + "</h3>";
+//                  // document.getElementById('cid').innerHTML = j;
+//          
+//                   //ate and draw the chart.
+//                   var chart = new google.visualization.LineChart(document.getElementById('myLineChart'));
+//                   chart.draw(data, null);
+//               }
+          
+        //  console.log("Outputs data" + outputobj.O)
+        
+          for(var i in outputobj){
+            console.log("objects are");
+            console.log(i);
+          }
+        //  res.render('graphvalues',data);
+
+         // req.flash(JSON.stringify(outputobj));
+       //   res.end(JSON.stringify(outputobj));
+          //res.redirect('/');
+         // var context = JSON.stringify(outputobj);
+          res.render('buckConverter',{outputobj : outputobj, Vo : Vo,Io : Io, P : P,deltaIL : deltaIL});
+
+        	client.destroy(); // kill client after server's response
+        });
+
+
+
+
+
+        client.on('error',function(ex) {
+          console.log("handled error");
+          console.log(ex);
+        }
+      );
+
+      //  res.end(JSON.stringify(data));
+
+        client.on('close', function() {
+        	console.log('Connection closed');
+        });
+        
+       
+
+});
+
 
 router.get('/logout', function(req, res){
 	console.log('got into router logout get function users');
