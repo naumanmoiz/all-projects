f93dcf3ee4845f0ba9e35aed2fe05effc3a75ea1
Yashwanth Siripragada <ysiripragada@gmail.com>
2018-11-14 10:57:46 -0600

    Add new graph to Buck Converter.

--- public/javascripts/experiments/buckconverter.js
+++ public/javascripts/experiments/buckconverter.js
@@ -29,11 +29,14 @@ function handleComponentDrop(event, ui) {
 	var s21 = str1.slice(2, 3);
 	var s22 = str2.slice(0, 1);
 
+	var s31 = str1.slice(0, 1);
+	var s32 = str2.slice(2, 3);
+
 	sa.push(s21);
 	sb.push(s22);
 
-	console.log(s21);
-	console.log(s22);
+	console.log(s31);
+	console.log(s32);
 	if (s11 == s12) {
 		ui.draggable.addClass('correct');
 		ui.draggable.draggable('disable');
@@ -41,6 +44,46 @@ function handleComponentDrop(event, ui) {
 		ui.draggable.position({ of: $(this), my: 'left top', at: 'left top' });
 		ui.draggable.draggable('option', 'revert', false);
 		total++;
+		placeComponent(s31, s32);
+	}
+}
+
+function placeComponent (s31, s32)
+{
+	if (s31 == "a")
+	{
+		var s = s32.charCodeAt(0) + 7;
+		document.getElementById(String.fromCharCode(s)).style.backgroundImage = 'url(https://upload.wikimedia.org/wikipedia/commons/c/c9/Resistor_v_wikisch.svg)';
+	}
+	else if (s31 == "b")
+	{
+		var s = s32.charCodeAt(0) + 7;
+		document.getElementById(String.fromCharCode(s)).style.backgroundImage = 'url(https://upload.wikimedia.org/wikipedia/commons/a/a7/Inductor_h_wikisch.svg)';	
+	}
+	else if (s31 == "c")
+	{
+		var s = s32.charCodeAt(0) + 7;
+		document.getElementById(String.fromCharCode(s)).classList.add('rotateimg90minus');
+		document.getElementById(String.fromCharCode(s)).style.backgroundImage = 'url(https://upload.wikimedia.org/wikipedia/commons/c/cc/Diode01.svg)';	
+	}
+	else if (s31 == "d")
+	{
+		var s = s32.charCodeAt(0) + 7;
+		document.getElementById(String.fromCharCode(s)).style.backgroundImage = 'url(/images/experiments/buckConverter/npn_symbol.svg)';
+	}
+	else if (s31 == "e")
+	{
+		var s = s32.charCodeAt(0) + 7;
+		document.getElementById(String.fromCharCode(s)).style.backgroundImage = 'url(https://upload.wikimedia.org/wikipedia/commons/3/38/Capacitor_Down_wikisch.svg)';
+	}
+	else if (s31 == "f")
+	{
+		var s = s32.charCodeAt(0) + 7;
+		document.getElementById(String.fromCharCode(s)).style.backgroundImage = 'url(https://upload.wikimedia.org/wikipedia/commons/a/a3/Battery_symbol1.svg)';
+	}
+	else if (s31 == "g")
+	{
+		var s = s32.charCodeAt(0) + 7;
 	}
 }
 
